d3dec3de9596de79c90b2e34e42182ed4244eb4e
Luis Hartmann <GlasFrost@users.noreply.github.com>
2014-12-26 19:50:07 +0100

    Update mucbot-greeting.py

--- mucbot-greeting.py
+++ mucbot-greeting.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-SleekXMPP: The Sleek XMPP Library
-Copyright (C) 2010 Nathanael C. Fritz
-This file is part of SleekXMPP.
-See the file LICENSE for copying permission.
-"""
+
 import sys
 import logging
 import getpass
